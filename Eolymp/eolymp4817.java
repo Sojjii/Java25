5633957eb8b1bf90a6f51d714098241e68d877b1
Sojjii <demchenkosofiya181@gmail.com>
2025-12-03 23:10:07 +0200

    task 14

---
+++ Eolymp/eolymp4817.java
@@ -0,0 +1,17 @@
+package Eolymp;
+import java.util.Scanner;
+public class eolymp4817 {
+
+
+
+        public static void main(String[] args) {
+            Scanner console = new Scanner(System.in);
+            int x = console.nextInt();
+            if (x<5) {
+                System.out.println(x*x-3*x+4);
+            } else {
+                System.out.println(x+7);
+            }
+        }
+    }
+
